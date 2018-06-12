54ceba0c366f047554c949f4779c3231cb8cf3db
flyingkisser <flyingkisser@163.com>
2018-06-12 23:20:08 +0800

    ff

---
+++ extensions/ccsp/FlowField.cpp
@@ -0,0 +1,13 @@
+//
+//  flowField.cpp
+//  libcocos2d Mac
+//
+//  Created by Joe on 2018/6/12.
+//
+
+#include "FlowField.h"
+
+use namespace ccsp;
+void *FlowField::doParseByPFTable(){
+    
+};
---
+++ extensions/ccsp/FlowField.h
@@ -0,0 +1,24 @@
+//
+//  flowField.hpp
+//  libcocos2d Mac
+//
+//  Created by Joe on 2018/6/12.
+//
+
+#ifndef FlowField_h
+#define FlowField_h
+
+#include <stdio.h>
+#include <string>
+#include "platform/CCPlatformMacros.h"
+#include "ui/GUIExport.h"
+#include "2d/CCNode.h"
+
+namespace  ccsp{
+    class FlowFiledUtil{
+    public:
+            static void* doParseByPFTable();
+    };
+}
+
+#endif /* FlowField_h */
