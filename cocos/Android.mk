3da69bb071a670c9f9d76a4150c7b8291153a2b2
flyingkisser <flyingkisser@163.com>
2018-07-09 19:04:49 +0800

    enable tiff

--- cocos/Android.mk
+++ cocos/Android.mk
@@ -244,7 +244,7 @@ LOCAL_EXPORT_LDLIBS := -lGLESv2 \
 LOCAL_STATIC_LIBRARIES := cocos_freetype2_static
 LOCAL_STATIC_LIBRARIES += cocos_png_static
 LOCAL_STATIC_LIBRARIES += cocos_jpeg_static
-#joe#LOCAL_STATIC_LIBRARIES += cocos_tiff_static
+LOCAL_STATIC_LIBRARIES += cocos_tiff_static
 LOCAL_STATIC_LIBRARIES += cocos_webp_static
 #joe#LOCAL_STATIC_LIBRARIES += cocos_chipmunk_static
 LOCAL_STATIC_LIBRARIES += cocos_zlib_static
@@ -292,7 +292,7 @@ $(call import-module,platform/android)
 $(call import-module,png/prebuilt/android)
 $(call import-module,zlib/prebuilt/android)
 $(call import-module,jpeg/prebuilt/android)
-#joe#$(call import-module,tiff/prebuilt/android)
+$(call import-module,tiff/prebuilt/android)
 $(call import-module,webp/prebuilt/android)
 #joe#$(call import-module,chipmunk/prebuilt/android)
 #joe#$(call import-module,3d)
