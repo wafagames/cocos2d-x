0cd894bae3af4a694289e7a6f5d3c051d6337c25
flyingkisser <flyingkisser@163.com>
2018-07-18 00:44:26 +0800

    UIUtil

--- extensions/ccsp/UIUtil.cpp
+++ extensions/ccsp/UIUtil.cpp
@@ -22,13 +22,6 @@ UIUtil* UIUtil::getInstance(){
     }
     return s_instance;
 }
-
-ui::Widget* UIUtil::seekWidgetByName(ui::Widget* root, const char* name){
-        return cocos2d::ui::Helper::seekWidgetByName(root, name);
-}
-ui::Widget* UIUtil::seekWidgetByTag(ui::Widget* root, int tag){
-    return cocos2d::ui::Helper::seekWidgetByTag(root, tag);
-}
 Node* UIUtil::seekNodeByName(Node* root, const std::string& name)
 {
     if (!root)
@@ -53,4 +46,62 @@ Node* UIUtil::seekNodeByName(Node* root, const std::string& name)
         }
     }
     return nullptr;
+}
+ui::Widget* UIUtil::seekWidgetByName(ui::Widget* root, const char* name){
+        return cocos2d::ui::Helper::seekWidgetByName(root, name);
+}
+ui::Widget* UIUtil::seekWidgetByTag(ui::Widget* root, int tag){
+    return cocos2d::ui::Helper::seekWidgetByTag(root, tag);
+}
+
+
+cocos2d::ui::ImageView* UIUtil::createImage(const char* szPath,Vec2 pos,bool scale9Enabled,int s_w,int s_h){
+    ui::ImageView* image;
+    if(szPath[0]=='#')
+        image=ui::ImageView::create(&szPath[1],ui::Widget::TextureResType::PLIST);
+    else
+         image=ui::ImageView::create(szPath,ui::Widget::TextureResType::LOCAL);
+    image->setIgnoreAnchorPointForPosition(false);
+    image->setAnchorPoint(Point(0.5,0.5));
+    image->setPosition(pos);
+    if(scale9Enabled)
+        image->setScale9Enabled(true);
+    if(s_w || s_h){
+        Size imgSize=image->getContentSize();
+        image->setCapInsets(Rect(s_w,s_h,imgSize.width-s_w*2,imgSize.height-s_h*2));
+    }
+    return image;
+}
+
+cocos2d::ui::Button* UIUtil::createBtn(const char* szPath,const char* str,Color3B c,const char* fontName,
+                                      int fontSize,bool scale9Enabled,int s_w,int s_h,Size size,Vec2 pos,
+                                       std::function<void(Ref*)> cb,bool bHide){
+    ui::Button* btnObj;
+    if(szPath[0]=='#')
+        btnObj=ui::Button::create(&szPath[1],&szPath[1],&szPath[1],ui::Widget::TextureResType::PLIST);
+    else
+        btnObj=ui::Button::create(szPath,szPath,szPath,ui::Widget::TextureResType::LOCAL);
+    if(strlen(str))
+        btnObj->setTitleText(str);
+    btnObj->setTitleColor(c);
+    if(strlen(fontName))
+        btnObj->setTitleFontName(fontName);
+    if(fontSize)
+        btnObj->setTitleFontSize(fontSize);
+    if(scale9Enabled && (s_w || s_h)){
+        btnObj->setScale9Enabled(true);
+        if(s_w || s_h){
+            Size imgSize=btnObj->getContentSize();
+            btnObj->setCapInsets(Rect(s_w,s_h,imgSize.width-s_w*2,imgSize.height-s_h*2));
+        }
+    }
+    btnObj->setContentSize(size);
+    btnObj->setPosition(pos);
+    btnObj->addClickEventListener(cb);
+    btnObj->setIgnoreAnchorPointForPosition(false);
+    btnObj->setAnchorPoint(Point(0.5,0.5));
+    btnObj->setPressedActionEnabled(true);
+    if(bHide)
+        btnObj->setVisible(false);
+    return btnObj;
 }
--- extensions/ccsp/UIUtil.h
+++ extensions/ccsp/UIUtil.h
@@ -10,18 +10,25 @@
 #define UIUtil_h
 
 #include <string>
+#include <functional>
 #include "platform/CCPlatformMacros.h"
 #include "ui/GUIExport.h"
 #include "2d/CCNode.h"
 #include "ui/CocosGUI.h"
 
+using namespace cocos2d;
+
 namespace  ccsp{
     class UIUtil{
     public:
         static UIUtil* getInstance();
-        cocos2d::Node* seekNodeByName(cocos2d::Node* root, const std::string& name);
-        cocos2d::ui::Widget* seekWidgetByName(cocos2d::ui::Widget* root, const char* name);
-         cocos2d::ui::Widget* seekWidgetByTag(cocos2d::ui::Widget* root, int tag);
+        Node* seekNodeByName(Node* root, const std::string& name);
+        static cocos2d::ui::Widget* seekWidgetByName(cocos2d::ui::Widget* root, const char* name);
+        static cocos2d::ui::Widget* seekWidgetByTag(cocos2d::ui::Widget* root, int tag);
+        static cocos2d::ui::ImageView* createImage(const char* szPath,Vec2 pos,bool scale9Enabled,int s_w,int s_h);
+        static cocos2d::ui::Button* createBtn(const char* szPath,const char* str,Color3B c,const char* fontName,
+                                              int fontSize,bool scale9Enabled,int s_w,int s_h,Size size,Vec2 pos,
+                                              std::function<void(Ref*)>,bool bHide);
     };
 }
 
